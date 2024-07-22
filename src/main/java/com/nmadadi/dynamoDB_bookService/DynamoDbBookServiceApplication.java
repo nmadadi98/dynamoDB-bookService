dbfd607145f292f32cc50023d877a6d7f0bcf740
nmadadi98 <nithinmadadi1@gmail.com>
2024-07-22 17:10:53 -0400

    first commit

---
+++ src/main/java/com/nmadadi/dynamoDB_bookService/DynamoDbBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nmadadi.dynamoDB_bookService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDbBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoDbBookServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nmadadi/dynamoDB_bookService/DynamoDbBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmadadi.dynamoDB_bookService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbBookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
